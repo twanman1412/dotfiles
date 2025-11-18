1fe47bbd9f23cf202375c65700c2e124e1e27e50
Twan Kuipers <twankuipers@home.nl>
2025-11-18 09:26:06 +0100

    general: Added yay package list and updated install script

--- .packages/install_packages.sh
+++ .packages/install_packages.sh
@@ -18,6 +18,7 @@ for file in [0-9][0-9]-*; do
 	sudo pacman -S --noconfirm --needed $pkgs
 done
 
+echo "Installing flatpak"
 sudo pacman -S --noconfirm --needed flatpak
 
 echo "Installing flatpak packages"
@@ -25,7 +26,22 @@ fpkgs=$(tr '\n' ' ' < flatpak_packages)
 if [ -z "$fpkgs" ]; then
 	echo "	File is empty, skipping."
 else
-	echo "	Running: flatpak install -y $fpkgs"
+	echo "	Running: sudo flatpak install -y $fpkgs"
 	sudo flatpak install -y $fpkgs
+fi
+
+echo "Installing yay"
+sudo pacman -U --noconfirm --needed yay-12.5.2-2-x86_64.pkg.tar.zst
+
+echo "Updating yay repositories"
+yay -Syu
+
+echo "Installing yay packages"
+ypkgs=$(tr '\n' ' ' < yay_packages)
+if [ -z "$ypkgs" ]; then
+	echo "	File is empty, skipping."
+else
+	echo "	Running: yay -S --noconfirm --needed $ypkgs"
+	yay -S --noconfirm --needed $ypkgs
 fi
 
